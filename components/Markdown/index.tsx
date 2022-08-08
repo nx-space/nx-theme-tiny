4210c85f9208c2c7f20bb9a5bdbfffa9efd0776c
wibus-wee <1596355173@qq.com>
2022-08-08 18:17:25 +0800

    fix(markdown): React Hydration Error when rendering markdown

--- components/Markdown/index.tsx
+++ components/Markdown/index.tsx
@@ -3,11 +3,12 @@
  * @author: Wibus
  * @Date: 2022-08-08 16:01:35
  * @LastEditors: Wibus
- * @LastEditTime: 2022-08-08 18:10:38
+ * @LastEditTime: 2022-08-08 18:16:26
  * Coding With IU
  */
 
 
+import { useEffect, useState } from 'react'
 import ReactMarkdown from 'react-markdown'
 import rehypeKatex from 'rehype-katex'
 import gfm from 'remark-gfm'
@@ -17,18 +18,25 @@ import { CodeBlock } from './renderers/CodeBlock'
 import { Image } from './renderers/Image'
 export const Markdown = (props: { source: string, [key: string]: any }) => {
   props.images ? images.data = props.images : null
+
+  const [components, setComponents] = useState({})
+
+  useEffect(() => {
+    setComponents({
+      'img': Image,
+      'pre': CodeBlock,
+      'code': CodeBlock,
+    })
+  }, [])
+
   return (
     <div id="write">
       <ReactMarkdown
-      remarkPlugins={[
-        gfm,
-        rehypeKatex
-      ]}
-      components={isClientSide() && {
-        // 'img': Image,
-        // 'pre': CodeBlock,
-        // 'code': CodeBlock,
-      } || {}}
+        remarkPlugins={[
+          gfm,
+          rehypeKatex
+        ]}
+        components={isClientSide() && components || {}}
       >
         {props.source}
       </ReactMarkdown>
---
+++ components/widgets/Comments/index.tsx
@@ -0,0 +1,8 @@
+/*
+ * @FilePath: /nx-theme-tiny/components/widgets/Comments/index.tsx
+ * @author: Wibus
+ * @Date: 2022-08-08 18:14:29
+ * @LastEditors: Wibus
+ * @LastEditTime: 2022-08-08 18:14:30
+ * Coding With IU
+ */
