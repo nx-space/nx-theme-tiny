4e5f32857d099633f4067e01153443d7423afd0d
Wibus <1596355173@qq.com>
2022-08-08 14:02:04 +0800

    perf(request): just show error

--- utils/request.util.ts
+++ utils/request.util.ts
@@ -9,13 +9,13 @@ import appState from "../states/appState";
 export const apiClient = $fetch.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
   retry: 3,
-  async onRequestError({ request, error }) {
-    console.log('[fetch request error]', request, error)
-    message.error(`[Fetch 请求错误] ${request} ${error}`)
+  async onRequestError({ error }) {
+    console.log('[fetch request error]', error)
+    message.error(`[Fetch 请求错误] ${error}`)
   },
-  async onResponseError({ request, error }) {
-    console.log('[fetch response error]', request, error)
-    message.error(`[Fetch 返回错误] ${request} ${error}`)
+  async onResponseError({ error }) {
+    console.log('[fetch response error]', error)
+    message.error(`[Fetch 返回错误] ${error}`)
   },
   headers: {
     'Content-Type': 'application/json',
